e9d57640f896e42eb8e19bcb2a6d18aabf60115b
Janghiro <jungyk3645@naver.com>
2024-06-20 22:08:59 +0900

    겜엔실_유니티기말제출

---
+++ Assets/@Scripts/Controllers/BaseController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class BaseController : MonoBehaviour
+{
+	protected bool _init = false;
+	protected Coroutine _coWait;
+
+	void Start()
+	{
+		Init();
+	}
+
+	protected virtual bool Init()
+	{
+		if (_init)
+			return false;
+
+		_init = true;
+		return true;
+	}
+
+	protected void WaitFor(float seconds)
+	{
+		if (_coWait != null)
+			StopCoroutine(_coWait);
+
+		_coWait = StartCoroutine(CoWait(seconds));
+	}
+
+	IEnumerator CoWait(float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		_coWait = null;
+	}
+
+	
+}
